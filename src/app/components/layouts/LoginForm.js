98cb5151040312c3c4ed33c6f5204f733e386bbc
Joshua Uriel Meguiso <ju.meguiso@Joshuas-MacBook-Air.local>
2026-09-16 20:27:28 +0800

    refactor: simplify login logic with sample credentials

--- src/app/components/layouts/LoginForm.js
+++ src/app/components/layouts/LoginForm.js
@@ -4,10 +4,7 @@ import TextFieldGroup from "../commons/TextFieldGroup";
 import { useState } from "react";
 import Image from "next/image";
 import logo from "../../images/logo.png";
-import axios from "axios";
-import setAuthToken from "../../utilities/setAuthToken";
-import { jwtDecode } from "jwt-decode";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setAuthentication } from "../../redux/reducers/userSlice";
 import { useRouter } from "next/navigation";
 
@@ -16,11 +13,21 @@ const initialValues = {
   password: "",
 };
 
+const sampleCredentials = {
+  username: "admin",
+  password: "admin12345",
+};
+
+const sampleUser = {
+  username: sampleCredentials.username,
+  permissions: [],
+  suppliers: [],
+  companies: [],
+};
+
 export default function Page() {
   const [state, setState] = useState(initialValues);
   const [errors, setErrors] = useState({});
-
-  const auth = useSelector((state) => state.auth);
 
   const dispatch = useDispatch();
   const router = useRouter();
@@ -31,38 +38,17 @@ export default function Page() {
         <Form
           layout="vertical"
           onFinish={() => {
-            axios
-              .post("/api/users/login", { ...state })
-              .then(async (response) => {
-                const {
-                  token,
-                  permissions,
-                  suppliers = [],
-                  companies,
-                } = response.data;
+            if (
+              state.username !== sampleCredentials.username ||
+              state.password !== sampleCredentials.password
+            ) {
+              setErrors({ username: "Invalid demo credentials" });
+              return;
+            }
 
-                const lock_date_reponse = await axios.get(
-                  "/api/account-settings/lock_transaction_date"
-                );
-
-                localStorage.setItem("jwtToken", token);
-                setAuthToken(token);
-
-                const decoded = {
-                  ...jwtDecode(token),
-                  permissions,
-                  suppliers,
-                  companies,
-                  lock_date: lock_date_reponse.data?.value,
-                };
-
-                dispatch(setAuthentication(decoded));
-
-                router.push("/");
-              })
-              .catch((error) => {
-                setErrors(error.response?.data);
-              });
+            setErrors({});
+            dispatch(setAuthentication(sampleUser));
+            router.push("/");
           }}
         >
           <div style={{ textAlign: "center", marginBottom: 16 }}>
